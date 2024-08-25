d76fda77ffa56d6650287ea199925a988788125c
Wolity <astapovsasa6@gmail.com>
2024-08-25 20:36:28 +0200

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,28 @@
 <template>
-<header>header</header>
+  <header class="header">
+    <a class="header_left">StarDB</a>
+    <nav class="header_right">
+      <a class="right_link">Form</a>
+      <a class="right_link">Starships</a>
+    </nav>
+  </header>
 </template>
 
-<style lang = "scss">
-
+<style lang="scss">
+.header {
+  padding-top: 40px;
+  color: var(--color-white);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .header_left {
+    font-size:var(--size-text);
+  }
+  .header_right {
+    .right_link {
+        font-size: var(--size-elem);
+        margin-left: 30px;
+    }
+  }
+}
 </style>
